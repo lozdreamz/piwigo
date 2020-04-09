66d041f4e79deb3af4ee9dc028826389a455c5de
Lozd <lozd@fastmail.com>
2020-04-09 11:30:17 +0700

    Add proggressbar and errors info

--- piwigo_upload.py
+++ piwigo_upload.py
@@ -2,7 +2,8 @@
 
 from configparser import ConfigParser
 from pathlib import Path
-from .piwigo import Piwigo
+from tqdm import tqdm
+from piwigo import Piwigo
 
 config = ConfigParser()
 config.read('config')
@@ -13,21 +14,38 @@ if 'default' in config:
 else:
     exit('Config file not found')
 
+print('Login to Piwigo...', end='')
 p = Piwigo(host, username, password)
 if p.is_ok:
-    album_name = Path.cwd().stem
+    print(' OK')
+    work_dir = Path.cwd()
+    album_name = work_dir.stem
     print(f'Create album "{album_name}"...', end='')
     album_id = p.create_album(album_name)
     if album_id != 0:
         print(' OK')
         # pathlib in Linux is case sensitive ¯\_(ツ)_/¯
-        files = list(Path.cwd().glob('*.jpg')) + list(Path.cwd().glob('*.JPG'))
+        ext = ['jpg', 'JPG', 'jpeg', 'JPEG']
+        files = []
+        for e in ext:
+            files.extend(list(work_dir.glob(f'*.{e}')))
         print(f'Uploading {len(files)} photos...')
-        for f in files:
-            p.upload(f, album_id)
-            if p.is_ok:
-                print('.')
-            else:
-                print('-')
+        errors = []
+        with tqdm(total=len(files),
+                  bar_format='{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}',
+                  ascii=' #') as pbar:
+            for f in files:
+                pbar.write(str(f))
+                p.upload(f, album_id)
+                pbar.update(1)
+                if not p.is_ok:
+                    errors.append(f)
+        n_err = len(errors)
+        print(f'\nCompleted with {n_err} error{"" if n_err==1 else "s"}.')
+        if n_err:
+            print('Failed to upload files: ')
+            for e in errors:
+                print(str(e))
+
 
 input('Press ENTER to continue...')
